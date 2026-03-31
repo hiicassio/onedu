bda42ef43a1adea6f6150e6a6be677a0a6db273d
LenilsonLima <lenilson.pantoja@estudante.ifms.edu.br>
2026-03-31 12:28:52 -0400

    31

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import DrawerComponent from './components/DrawerComponent';
 import AnaliseCandidato from './pages/analise_candidato/AnaliseCandidato';
 import InfoIntegracaoGestaoPresente from './pages/gestao_matricula/InfoIntegracaoGestaoPresente';
+import ControleMatriculaSimples from './pages/listagem/controle_matricula_simples/ControleMatriculaSimples';
 
 const styles = {
   backgroundColor: '#197DFF',
@@ -16,23 +17,24 @@ function App() {
   const [openCloseDrawerInfoIntegracaoGestaoPresente, setOpenCloseDrawerInfoIntegracaoGestaoPresente] = useState(false);
 
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia cum enim maxime velit nam perspiciatis ratione, consectetur obcaecati cupiditate voluptatum nesciunt beatae, voluptate pariatur provident, repudiandae cumque ab ipsa dolor?</p>
-      <br />
-      <button style={styles} onClick={() => setOpenCloseDrawer(true)}>Open Drawer AnaliseCandidato</button>
-      <br />
-      <br />
-      <button style={styles} onClick={() => setOpenCloseDrawerInfoIntegracaoGestaoPresente(true)}>Open Drawer InfoIntegracaoGestaoPresente</button>
-      <AnaliseCandidato
-        openCloseDrawer={openCloseDrawer}
-        setOpenCloseDrawer={setOpenCloseDrawer}
-      />
+    // <div>
+    //   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia cum enim maxime velit nam perspiciatis ratione, consectetur obcaecati cupiditate voluptatum nesciunt beatae, voluptate pariatur provident, repudiandae cumque ab ipsa dolor?</p>
+    //   <br />
+    //   <button style={styles} onClick={() => setOpenCloseDrawer(true)}>Open Drawer AnaliseCandidato</button>
+    //   <br />
+    //   <br />
+    //   <button style={styles} onClick={() => setOpenCloseDrawerInfoIntegracaoGestaoPresente(true)}>Open Drawer InfoIntegracaoGestaoPresente</button>
+    //   <AnaliseCandidato
+    //     openCloseDrawer={openCloseDrawer}
+    //     setOpenCloseDrawer={setOpenCloseDrawer}
+    //   />
 
-      <InfoIntegracaoGestaoPresente
-        openCloseDrawer={openCloseDrawerInfoIntegracaoGestaoPresente}
-        setOpenCloseDrawer={setOpenCloseDrawerInfoIntegracaoGestaoPresente}
-      />
-    </div>
+    //   <InfoIntegracaoGestaoPresente
+    //     openCloseDrawer={openCloseDrawerInfoIntegracaoGestaoPresente}
+    //     setOpenCloseDrawer={setOpenCloseDrawerInfoIntegracaoGestaoPresente}
+    //   />
+    // </div>
+    <ControleMatriculaSimples />
   )
 }
 
